7e3c8d71593eb0e36fc18625b24aee71951b8bb7
Yes <ilarumbgim1@educacion.navarra.es>
2020-10-15 06:29:36 +0000

    more pog updates

--- index.js
+++ index.js
@@ -3,11 +3,29 @@ const config = require('./config')
 const helmet = require('helmet')
 const app = express();
 var exphbs = require('express-handlebars');
+var path = require('path')
 app.use(helmet());
 app.set('view engine', 'handlebars');
 
+app.use('/static', express.static(path.join(__dirname, 'views/public')))
+
 
 app.engine('handlebars', exphbs());
+app.get('/games', (req, res) => {
+	if (config.maintanencemode == true) {
+		console.log("TEST")
+		res.send(config.maintanencetitle)
+
+	}
+	res.render('games', {
+		helpers: {
+			alertinfo: config.alertText,
+			alertEnabled: config.alertEnabled,
+		}
+	});
+
+
+})
 app.get('/', (req, res) => {
 	if (config.maintanencemode == true) {
 		console.log("TEST")
