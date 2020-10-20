dc029bd0ee4f5d7ec170ad840c4f8320ea9e7ff2
Yes <ilarumbgim1@educacion.navarra.es>
2020-10-20 09:34:38 +0000

    big big boi updated

--- index.js
+++ index.js
@@ -4,91 +4,118 @@ const helmet = require('helmet')
 const app = express();
 var exphbs = require('express-handlebars');
 var path = require('path')
+const Database = require("@replit/database")
+const db = new Database()
+
+
+
 app.use(helmet());
 app.set('view engine', 'handlebars');
 
 app.use('/static', express.static(path.join(__dirname, 'views/public')))
-
 app.enable('view cache');
 
 app.engine('handlebars', exphbs());
 
 app.get('/games', (req, res) => {
-    if (config.maintanencemode == true) {
-        res.render('maintanence', {
-            layout: false,
+	if (config.maintanencemode == true) {
+		res.render('maintanence', {
+			layout: false,
 
-            helpers: {
-		    alertTitle: config.alertTitle,
-                alertinfo: config.alertText,
-                alertEnabled: config.alertEnabled,
-            }
-        });
-    } else {
-        res.render('games', {
-            helpers: {
-		alertTitle: config.alertTitle,
-                alertinfo: config.alertText,
-                alertEnabled: config.alertEnabled,
-            }
-        });
-    }
+			helpers: {
+				alertTitle: config.alertTitle,
+				alertinfo: config.alertText,
+				alertEnabled: config.alertEnabled,
+			}
+		});
+	} else {
+		res.render('games', {
+			helpers: {
+				alertTitle: config.alertTitle,
+				alertinfo: config.alertText,
+				alertEnabled: config.alertEnabled,
+			}
+		});
+	}
+
+
+})
+app.get('/youtube', (req, res) => {
+	if (config.maintanencemode == true) {
+		res.render('maintanence', {
+			layout: false,
+
+			helpers: {
+				alertTitle: config.alertTitle,
+				alertinfo: config.alertText,
+				alertEnabled: config.alertEnabled,
+			}
+		});
+	} else {
+		res.render('youtube', {
+			helpers: {
+				alertTitle: config.alertTitle,
+				alertinfo: config.alertText,
+				alertEnabled: config.alertEnabled,
+			}
+		});
+	}
 
 
 })
 
 app.get('/blog', (req, res) => {
-    if (config.maintanencemode == true) {
-        res.render('maintanence', {
-            layout: false,
+	if (config.maintanencemode == true) {
+		res.render('maintanence', {
+			layout: false,
 
-            helpers: {
-		    		    alertTitle: config.alertTitle,
+			helpers: {
+				alertTitle: config.alertTitle,
 
-                alertinfo: config.alertText,
-                alertEnabled: config.alertEnabled,
-            }
-        });
-    } else {
-        res.render('blog', {
-            helpers: {
-		    		    alertTitle: config.alertTitle,
+				alertinfo: config.alertText,
+				alertEnabled: config.alertEnabled,
+			}
+		});
+	} else {
+		res.render('blog', {
+			helpers: {
+				alertTitle: config.alertTitle,
 
-                alertinfo: config.alertText,
-                alertEnabled: config.alertEnabled,
-            }
-        });
-    }
+				alertinfo: config.alertText,
+				alertEnabled: config.alertEnabled,
+			}
+		});
+	}
 
 
 })
 
 app.get('/', (req, res) => {
-    if (config.maintanencemode == true) {
-        res.render('maintanence', {
-            layout: false,
-            helpers: {
-		    		    alertTitle: config.alertTitle,
+	if (config.maintanencemode == true) {
+		res.render('maintanence', {
+			layout: false,
+			helpers: {
+				alertTitle: config.alertTitle,
 
-                alertinfo: config.alertText,
-                alertEnabled: config.alertEnabled,
-            }
-        });
-    } else {
-        res.render('home', {
-            helpers: {
-		    		    alertTitle: config.alertTitle,
+				alertinfo: config.alertText,
+				alertEnabled: config.alertEnabled,
+			}
+		});
+	} else {
+		res.render('home', {
+			helpers: {
+				alertTitle: config.alertTitle,
 
-                alertinfo: config.alertText,
-                alertEnabled: config.alertEnabled,
-            }
-        });
-    }
+				alertinfo: config.alertText,
+				alertEnabled: config.alertEnabled,
+			}
+		});
+	}
 })
 
 
 
 
 app.listen(3000, () => {
-    console.log('server started');
+	console.log('server started');
 });
